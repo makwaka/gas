2778a776669446521146557ac64849087ac648c6
Phariq <pharisnjuguna22@gmail.com>
2023-01-12 22:44:18 +0300

    A gas delivery app

---
+++ index.js
@@ -0,0 +1,114 @@
+ const products = [
+    {
+        id: 1,
+        title: '13kg Supa-gas',
+        img: "./images/13kg-supagas.png",
+        price: "Ksh. 6000",
+    },
+      {
+        id: 2,
+        title: '13kg K-gas',
+        img: "./images/13kg-kgas.jpg",
+        price: "Ksh. 6000",
+    },
+      {
+        id: 3,
+        title: '6kg Hashi gas',
+        img: "./images/6kg-hashigas.jpg",
+        price: "Ksh. 3000",
+    },
+      {
+        id: 4,
+        title: '6kg SEA gas',
+        img: "./images/6kg-seagas.jpg",
+        price: "Ksh. 3000",
+    },
+      {
+        id: 5,
+        title: '13kg SEA gas',
+        img: "./images/13kg-seagas.jpg",
+        price: "Ksh. 6000",
+    },
+      {
+        id: 6,
+        title: '6kg Oilibya Mpishi',
+        img: "./images/6kg-mpishigas.jpg",
+        price: "Ksh. 3000",
+    },
+      {
+        id: 7,
+        title: '13kg Oilibya Mpishi',
+        img: "./images/13kg-mpishigas.jpg",
+        price: "Ksh. 6000",
+    },
+      {
+        id: 8,
+        title: '6kg Total',
+        img: "./images/6kg-total.jpg",
+        price: "Ksh. 3000",
+    },
+      {
+        id: 9,
+        title: '13kg Total',
+        img: "./images/13kg-total.png",
+        price: "Ksh. 6000",
+    },
+      {
+        id: 10,
+        title: '13kg M-Gas',
+        img: "./images/13kg-mgas.png",
+        price: "Ksh. 6000",
+    },
+      {
+        id: 11,
+        title: '6kg Mid-Gas',
+        img: "./images/6kg-midgas.jpg",
+        price: "Ksh. 3000",
+    },
+      {
+        id: 12,
+        title: '6kg handigas',
+        img: "./images/6kg-handigas.png",
+        price: "Ksh. 3000",
+    },
+ ];
+
+// select items
+ const items = document.querySelector('.items');
+const searchInput = document.querySelector('#search-input');
+
+
+
+// search items
+
+searchInput.addEventListener('keyup', (e)=>{
+const value = e.target.value;
+const searchQuery = value.toLowerCase();
+
+for(const product of products){
+if(String(product).includes(searchQuery)){
+  String(product).style.display = 'block';
+}
+}
+})
+
+//load items
+ window.addEventListener('DOMContentLoaded', () =>{
+displayGasItems(products);
+ });
+
+ function displayGasItems(gasItems){
+let displayProducts = gasItems.map((product) => {
+        // console.log(product);
+        return `<div id="description">
+        <img src=${product.img} alt="gas image" loading="lazy">
+        <div id="info">
+        <h3>${product.title}</h3>
+        <hr id="line">
+        <p>${product.price}</p>
+        </div>
+    </div>`;
+    })
+    displayProducts = displayProducts.join('');
+    items.innerHTML = displayProducts;
+ };
